ea54c22be183b0725c3b88d40c6b39aa69a79ed2
cibo94 <xcibul10@stud.fit.vutbr.cz>
2014-12-30 10:06:48 +0100

    Upravenestuff

--- index.php
+++ index.php
@@ -12,12 +12,13 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <title>TRI ORIEŠKY</title>
         <meta http-equiv="content-type" content="text/html; charset=UTF-8">
         <meta name="description" content="..." />
         <meta name="keywords" content="..." />
         <meta name="author" content="Miroslav Cibulka, Jana Cibulkova" />
-        <title>TRI ORIEŠKY</title>
         <link rel="stylesheet" type="text/css" href="styles/default.css" />
+        <link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
         <script type="text/javascript" src="scripts/request.js"></script>
         <script type="text/javascript" src="scripts/gallery.js"></script>
         <script type="text/javascript">
