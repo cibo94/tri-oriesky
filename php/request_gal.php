34bbf36a887b7b34200ea42d7c9eeb9873ad49b3
Miroslav Cibulka <xcibul10@stud.fit.vutbr.cz>
2015-07-10 22:37:08 +0200

    .htaccess added, favicon changed, few changes

--- php/request_gal.php
+++ php/request_gal.php
@@ -14,7 +14,7 @@
         if (is_file($gallery_dir.$path) && is_regular_jpg(substr($path, -3))) {
             $image_out .= '<a href="'.$gallery_subdir.$path.'" rel="lightbox-cast" title="'. substr($path, 0, -4) .'"><img src="'.$gallery_subdir.$path.'" alt="'. substr($path, 0, -4) .'"  class="gallery-image" hspace="20" vspace="20" /></a>';
         } elseif (is_dir($gallery_dir.$path) && $path != "." && $path != "..") {
-            $directory_out .= '<h2><a href="?page=galeria.html&gal_dir='.$path.'" class="galery-button">'.$path.'</a></h2>';
+            $directory_out .= '<h2><a href="galeria/'.$path.'" class="galery-button">'.$path.'</a></h2>';
         }
     }
     echo $directory_out . $image_out;
